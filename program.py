ed010f52a121a4c39414a301e11089328ebf8e33
Dhilip777 <dilebanm@gmail.com>
2023-06-14 18:44:08 +0530

    add some extra code

--- program.py
+++ program.py
@@ -1,5 +1,8 @@
+print("Task 1")
 celsius=float(input("Enter the input for celsius = "))
 a=(celsius*1.8)+32
 print(a)
-print("Hello world")
-print("Enter into python world")
+print("Task 2")
+fahrenheit=float(input("Enter the input for fahrenheit = "))
+b=((fahrenheit-32)*5)/9
+print(b)
