ffa016a2b871302f8223bc6fbc3d885116f50e83
Dhilip777 <132293468+Dhilip777@users.noreply.github.com>
2023-06-21 16:04:53 +0530

    Update program.py

--- program.py
+++ program.py
@@ -5,7 +5,8 @@ print(a)
 
 print("Hello world")
 print("conflict from branch")
-
+def calculate():
+    print("Task 1")
 print("Task 2")
 fahrenheit=float(input("Enter the input for fahrenheit = "))
 b=((fahrenheit-32)*5)/9
